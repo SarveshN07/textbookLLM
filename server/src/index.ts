fce2faa4b3d4bdf93a310a1245053b1d27ab2492
Sarvesh <sarveshnirmalkar@gmail.com>
2026-08-05 12:10:13 +0530

    add workspace logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,20 @@ import "dotenv/config"
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
 const app = express();
+import cors from "cors";
+import { registerRoutes } from "./routes/index.js";
+import { errorHandler } from "./middlewares/error-handler.middleware.js";
+
+
+const clientUrl = process.env.CLIENT_URL ?? "http://localhost:3001";
+
+
+app.use(
+    cors({
+        origin: clientUrl,
+        credentials: true,
+    }),
+);
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 app.use(express.json());
@@ -15,6 +29,11 @@ app.get("/", (req , res) => {
 app.get("/health" , (req,res) => {
     res.status(200).json({status : "ok"})
 })
+
+
+registerRoutes(app);
+
+app.use(errorHandler)
 
 app.listen(process.env.PORT , () => {
     console.log(`Server is listening on  http://localhost:${process.env.PORT}`)
